008372d8cc3d58b81dd8f7254f5a6c4cd9035679
Dzirx <92047124+Dzirx@users.noreply.github.com>
2022-11-23 16:16:27 +0100

    Add files via upload

---
+++ public/quiz_data/quiz_data.js
@@ -0,0 +1,22 @@
+export default {
+    title: "Quiz matematyczny - zaawansowany",
+    questions: [
+        {
+            formula: "1+1",
+            answer: 2,
+            timeSpent: 0,
+        },
+        {
+            formula: "5*5", answer: 25, odp: 0, timeSpent: 0
+        },
+        {
+            formula: "100/25", answer: 4, odp: 0, timeSpent: 0
+        },
+        {
+            formula: "2*25", answer: 50, odp: 0, timeSpent: 0
+        },
+        {
+            formula: "4/4", answer: 1, odp: 0, timeSpent: 0
+        }
+    ]
+};
